2cf2c1f3265907930e860ae14914e70ac168d578
Francisco Martins <fmaraposo@gmail.com>
2023-06-22 00:06:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calendar from './components/Calendar/Calendar'
+
+function App() {
+
+	return (
+		<>
+			<h1>Helloooo</h1>
+      <Calendar />
+		</>
+	)
+}
+
+export default App
