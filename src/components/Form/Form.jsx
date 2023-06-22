093059e17607d8c1ca5c3cbb398a17a92424d61a
Francisco Martins <fmaraposo@gmail.com>
2023-06-22 17:23:20 +0100

    Add react hooks form to handle the event form and its validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,8 @@
-import * as React from 'react'
-import { Modal, TextField } from '@mui/material'
+import React from 'react'
+import { Modal, TextField, Button } from '@mui/material'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import {
-	TimePicker,
-	LocalizationProvider,
-	DatePicker,
-} from '@mui/x-date-pickers'
+import { useForm, Controller } from 'react-hook-form'
+import { TimePicker, LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
 
 const inputStyles = {
 	margin: 1,
@@ -13,7 +10,19 @@ const inputStyles = {
 }
 
 const FormModal = ({ openModal, setOpenModal }) => {
+	const {
+		register,
+		handleSubmit,
+		control,
+		formState: { errors },
+	} = useForm()
 	const handleClose = () => setOpenModal(false)
+
+	console.log('error', errors)
+
+	const onSubmit = (data) => {
+		console.log('Data To Submit', data)
+	}
 
 	return (
 		<Modal open={openModal} onClose={handleClose}>
@@ -25,32 +34,71 @@ const FormModal = ({ openModal, setOpenModal }) => {
 					<h3>Add your event</h3>
 				</div>
 				<div className="modal-body">
-					<TextField
-						defaultValue=""
-						label="Event Title"
-						variant="standard"
-						sx={inputStyles}
-					/>
-					<div className="date-pickers">
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<TextField
+							defaultValue=""
+							{...register('eventTitle', { required: true })}
+							error={Boolean(errors.eventTitle)}
+							helperText={errors.eventTitle && 'Event Title is required'}
+							label="Event Title"
+							variant="outlined"
+							sx={inputStyles}
+						/>
 						<LocalizationProvider dateAdapter={AdapterDateFns}>
-							<DatePicker label="Basic date picker" sx={{ ...inputStyles, width: '45%' }} />
+							<Controller
+								name="eventDate"
+								control={control}
+								rules={{ required: true }}
+								render={({ field: { onChange, value } }) => (
+									<DatePicker
+										label="Event Date"
+										value={value}
+										onChange={(date) => onChange(date)}
+										sx={inputStyles}
+										slotProps={{
+											textField: {
+												helperText: errors?.eventDate ? 'Event Date is required' : '',
+											},
+										}}
+									/>
+								)}
+							/>
 						</LocalizationProvider>
 						<LocalizationProvider dateAdapter={AdapterDateFns}>
-							<TimePicker label="Basic time picker" sx={{ ...inputStyles, width: '42%' }}/>
+							<Controller
+								name="eventTime"
+								control={control}
+								rules={{ required: true }}
+								render={({ field }) => (
+									<TimePicker
+										label="Event Time"
+										value={field.value}
+										onChange={(date) => field.onChange(date)}
+										sx={inputStyles}
+										slotProps={{
+											textField: {
+												helperText: errors?.eventTime ? 'Event Date is required' : '',
+											},
+										}}
+									/>
+								)}
+							/>
 						</LocalizationProvider>
-					</div>
-					<TextField
-						defaultValue=""
-						label="User"
-						variant="standard"
-						sx={inputStyles}
-					/>
-					<TextField
-						defaultValue=""
-						label="Notes"
-						variant="standard"
-						sx={inputStyles}
-					/>
+
+						<TextField
+							{...register('user', { required: true })}
+							error={Boolean(errors.user)}
+							helperText={errors.user && 'User is required'}
+							defaultValue=""
+							label="User"
+							variant="outlined"
+							sx={inputStyles}
+						/>
+						<TextField defaultValue="" label="Notes" variant="outlined" sx={inputStyles} />
+						<Button type="submit" variant="contained">
+							Submit
+						</Button>
+					</form>
 				</div>
 			</div>
 		</Modal>
