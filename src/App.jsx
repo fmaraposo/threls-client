4c7c644d1041c0abcfc45a1aa3bccfa178425a80
Francisco Martins <fmaraposo@gmail.com>
2023-06-23 12:36:36 +0100

    set date from the App.js and pass as props to children

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import { Box } from '@mui/material'
 import Calendar from './components/Calendar/Calendar'
 import Events from './components/Events/EventsList'
 import './App.css'
 
 function App() {
+	const [date, setDate] = useState(new Date())
+
 	return (
 		<>
 			<Box className="app-container">
-				<Events />
-				<Calendar />
+				<Events date={date}/>
+				<Calendar setDate={setDate} date={date} />
 			</Box>
 		</>
 	)
