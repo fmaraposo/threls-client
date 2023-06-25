72a2309b8d85dc6131b585a9150a37b2bd2279c5
Francisco Martins <fmaraposo@gmail.com>
2023-06-25 21:31:22 +0100

    Add day and week day on event list

--- src/components/Events/EventsList.jsx
+++ src/components/Events/EventsList.jsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import EventItem from './EventItem'
-import threlsLogo from '../../assets/threls.svg'
 import { getEventsOfToday } from '../../utils/RequestFunctions'
 
 const EventsList = ({ date }) => {
 	const [events, setEvents] = useState([])
-
 	useEffect(() => {
-		async function getTodayEvents() {
-			const response = await getEventsOfToday()
+		async function getTodayEvents(date) {
+			const response = await getEventsOfToday(date)
 			setEvents(response)
 			return response
 		}
-		getTodayEvents()
+		getTodayEvents(date)
 	}, [date])
+
+	const getWeekDay = (date) => {
+		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+		return days[date.getDay()]
+	}
 
 	return (
 		<div className="events-list-container">
-			<div className="logo-container">
-				<img src={threlsLogo} alt="threls logo" className="logo" width={'40%'} />
-			</div>
 			<section className="list-events-container">
-				{events.length === 0 ? (
-					<div className="no-events-container">
-						<h2>You don't have any events yet, please click on the calendar an add one</h2>
-					</div>
-				) : (
+				<div className='events'>
+					<h1 className='day'>{date.getUTCDate()}</h1>
+					<span className='weekDay'>{getWeekDay(date)}</span>
+				</div>
+				{events.length > 0 && (
 					<ul className="list-events">
-						<EventItem />
+						<EventItem events={events} />
 					</ul>
 				)}
 			</section>
