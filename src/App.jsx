8a2bf8117311e51099831ff2ee14e91874ad6baa
Francisco Martins <fmaraposo@gmail.com>
2023-06-25 21:30:23 +0100

    move logo to top left corner of application

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Box } from '@mui/material'
 import Calendar from './components/Calendar/Calendar'
 import Events from './components/Events/EventsList'
+import threlsLogo from './assets/threls.svg'
 import './App.css'
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
 
 	return (
 		<>
+			<img src={threlsLogo} alt="threls logo" className="logo" width={'15%'} />
 			<Box className="app-container">
-				<Events date={date}/>
+				<Events date={date} />
 				<Calendar setDate={setDate} date={date} />
 			</Box>
 		</>
