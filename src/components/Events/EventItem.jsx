6dfdd23c9f754ee66d278b6ee2d90935422b1554
Francisco Martins <fmaraposo@gmail.com>
2023-06-26 10:26:10 +0100

    handle delete on event item

--- src/components/Events/EventItem.jsx
+++ src/components/Events/EventItem.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Grow } from '@mui/material'
-import { format, parseISO } from 'date-fns'
+import { format, parseISO, isSameDay } from 'date-fns'
+import { Edit, DeleteOutline, Celebration, Check, Close } from '@mui/icons-material'
+import { deleteEvent } from '../../utils/RequestFunctions'
+import { handleState } from '../../utils/HandleStateFunctions'
 
-const EventItem = ({ events }) => {
+const EventItem = ({ events, setEvents, setOpenModal, setEditEvent }) => {
+	const [deleteItem, setDeleteItem] = useState(false)
+
 	return (
 		<Box sx={{ height: 180 }}>
 			<Box>
 				{events.map((event, index) => {
 					const eventIndex = events.findIndex((e) => e.id === event.id)
+					const isEventOnSameDay = isSameDay(parseISO(event.startDate), parseISO(event.endDate))
 					return (
 						<div key={index} style={{ overflow: 'scroll' }}>
 							<Grow
@@ -16,14 +22,56 @@ const EventItem = ({ events }) => {
 								timeout={eventIndex === index ? (index + 1) * 750 : 0}
 							>
 								<Box className="event-item-container">
-									<span className="event-title">{event.title}</span>
-									<span className="event-date start">
-										{format(parseISO(event.startDate), 'dd/MM/yyyy')}
-									</span>
-									<span className="event-date end">
-										{format(parseISO(event.endDate), 'dd/MM/yyyy')}
-									</span>
-									<span className="event-notes">{event.notes}</span>
+									<div className="event-item-details">
+										{deleteItem && deleteItem === event.id ? (
+											<span>Are you sure you want to delete this event ?</span>
+										) : (
+											<>
+												<span className="event-icon">
+													<Celebration />
+												</span>
+												<span className="event-title">{event.title}</span>
+												<span className="event-date date">
+													{isEventOnSameDay
+														? format(parseISO(event.startDate), 'dd/MM/yyyy')
+														: format(parseISO(event.startDate), 'dd/MM/yyyy') -
+														  format(parseISO(event.endDate), 'dd/MM/yyyy')}
+												</span>
+												<span className="event-date time">
+													{format(parseISO(event.startDate), 'HH:mm')} -{' '}
+													{format(parseISO(event.endDate), 'HH:mm')}
+												</span>
+
+												<span className="event-notes">{event.notes}</span>
+											</>
+										)}
+									</div>
+									<div className="event-item-actions">
+										{deleteItem && deleteItem === event.id ? (
+											<>
+												<Check
+													sx={{ marginRight: '20px' }}
+													onClick={async () => {
+														await deleteEvent(event.id)
+														setDeleteItem(false)
+														handleState(events, event, setEvents, 'delete')
+													}}
+												/>
+												<Close onClick={() => setDeleteItem(false)} />
+											</>
+										) : (
+											<>
+												<Edit
+													sx={{ marginRight: '20px' }}
+													onClick={() => {
+														setEditEvent(event)
+														setOpenModal(true)
+													}}
+												/>
+												<DeleteOutline onClick={() => setDeleteItem(event.id)} />
+											</>
+										)}
+									</div>
 								</Box>
 							</Grow>
 						</div>
