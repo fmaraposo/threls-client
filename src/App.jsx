f9cd2f769ed5359015b6799b9c30ce7c817f30e8
Francisco Martins <fmaraposo@gmail.com>
2023-06-26 10:25:59 +0100

    moving state to APP.jsx and pass it as props so able to handle on all components

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,31 @@ import './App.css'
 
 function App() {
 	const [date, setDate] = useState(new Date())
+	const [events, setEvents] = useState([])
+	const [editEvent, setEditEvent] = useState(null)
+	const [openModal, setOpenModal] = useState(false)
 
 	return (
 		<>
 			<img src={threlsLogo} alt="threls logo" className="logo" width={'15%'} />
 			<Box className="app-container">
-				<Events date={date} />
-				<Calendar setDate={setDate} date={date} />
+				<Events
+					date={date}
+					events={events}
+					setEvents={setEvents}
+					setOpenModal={setOpenModal}
+					setEditEvent={setEditEvent}
+				/>
+				<Calendar
+					setDate={setDate}
+					date={date}
+					events={events}
+					setEvents={setEvents}
+					openModal={openModal}
+					setOpenModal={setOpenModal}
+					editEvent={editEvent}
+					setEditEvent={setEditEvent}
+				/>
 			</Box>
 		</>
 	)
